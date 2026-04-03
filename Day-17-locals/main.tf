343e7db2cd48fc1676118dab003bee89db2363ce
Riya Singh <riyasingh@Riyas-MacBook-Air.local>
2026-04-04 03:52:36 +0530

    Day19

--- Day-17-locals/main.tf
+++ Day-17-locals/main.tf
@@ -1,7 +1,7 @@
 locals {
   region = "us-east-1"
   instance_type = "t2.micro"
-  ami_id = "ami-12345"
+  ami_id = "ami-02dfbd4ff395f2a1b"
 }
 resource "aws_instance" "name" {
     ami = local.ami_id
